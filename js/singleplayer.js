05540663b5bc30db48b568b4667872c90a6c00d9
MahshadOliaie <mahshadoliaie@gmail.com>
2023-08-18 21:48:54 +0330

    debug the single player ressualt show

--- js/singleplayer.js
+++ js/singleplayer.js
@@ -5,28 +5,31 @@ function chooseBox() {
 
     this.classList.add("O");
     this.classList.add("freeze");
-
+    linesContainer.classList.add("show")
     check();
     counter--;
     whooseTurn();
 
+    setTimeout(function(){linesContainer.classList.remove("show");} , 1000);
 
     if (turn.classList.length !== 4) {
-        setTimeout(systemTurn, 1000);
+        setTimeout(systemTurn, 1001);
     }
 }
 
 
 
 function systemTurn() {
+    
+
     let empty = [...document.querySelectorAll(".shape:not(.freeze)")];
     let random = Math.floor(Math.random() * empty.length);
     empty[random].classList.add("X");
     empty[random].classList.add("freeze");
-
     check();
     counter--;
     whooseTurn();
+    
 }
 
 for (const box of boxes) {
